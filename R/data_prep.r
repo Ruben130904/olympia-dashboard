be3b26c20953c3169cc844113d1cfb69c21eff8e
Ruben Gehlen <ruben.gehlen@stud.dshs-koeln.de>
2026-07-29 12:26:57 +0200

    Value Boxes echte Werte

--- R/data_prep.r
+++ R/data_prep.r
@@ -22,3 +22,42 @@ gender_by_year <- athlete_events %>%
 gender_by_year_female <- gender_by_year %>%
   filter(Sex == "F") %>%
   select(Year, Season, share)
+
+
+
+# --- Value-Box-Kennzahlen ---
+
+# Anzahl Teilnehmer*innen alltime (eindeutige Athlet*innen über alle Spiele)
+n_total <- athlete_events |> 
+  distinct(ID) |> 
+  nrow()
+
+# Neuestes Jahr im Datensatz
+latest_year <- max(athlete_events$Year)
+
+# Geschlechterverteilung im neuesten Jahr (eindeutige Athlet*innen)
+
+current_gender <- athlete_events |> 
+  filter(Year == latest_year) |> 
+  distinct(ID, Sex) |> 
+  count(Sex, name = "n") |> 
+  mutate(share = n / sum(n))
+
+n_women_current     <- current_gender %>% filter(Sex == "F") %>% pull(n)
+share_women_current <- current_gender %>% filter(Sex == "F") %>% pull(share)
+
+# Erstes Jahr, in dem Frauen teilgenommen haben
+first_year_women <- athlete_events |> 
+  filter(Sex == "F") |> 
+  pull(Year) |> 
+  min()
+
+# Geschlechterverteilung in diesem ersten Jahr
+first_year_gender <- athlete_events |> 
+  filter(Year == first_year_women) |> 
+  distinct(ID, Sex) |> 
+  count(Sex, name = "n") |> 
+  mutate(share = n / sum(n))
+
+n_women_first     <- first_year_gender %>% filter(Sex == "F") %>% pull(n)
+share_women_first <- first_year_gender %>% filter(Sex == "F") %>% pull(share)
